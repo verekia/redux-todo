e748dc369de033582f8f4a18e9def48278d2d426
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2021-08-28 00:09:40 +0700

    Add selectors

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
 import { useSelector, useDispatch } from 'react-redux'
 
-import { add, decrement, increment } from '../lib/redux'
+import { add, decrement, increment, countSelector, doubleCountSelector } from '../lib/redux'
 
 const IndexPage = () => {
   const dispatch = useDispatch()
-  const count = useSelector(s => s.count)
+  const count = useSelector(countSelector)
+  const doubleCount = useSelector(doubleCountSelector)
 
   return (
     <>
-      <h1 className="count">{count}</h1>
+      <h1 className="count">
+        {count} (double: {doubleCount})
+      </h1>
       <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
       <button onClick={() => dispatch(add(5))}>Add 5</button>
