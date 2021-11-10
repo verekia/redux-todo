9d2222184fc84c525f5e07f94229bcf4d3d862c4
Josephine-Ann <josephineanngeoghegan@gmail.com>
2021-11-10 13:26:44 +0100

    css modules, sass, mixins, jumping problem fixed

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { complete, add, view, remove, reading, tasks, tasksLengthAll, tasksLengthIncomplete, tasksLengthComplete } from '../lib/redux'
+// import { useState, useEffect } from 'react'
+import { useState } from 'react'
+// import { useSelector, useDispatch } from 'react-redux'
+// import { complete, add, view, remove, reading, tasks, tasksLengthAll, tasksLengthIncomplete, tasksLengthComplete } from '../lib/redux'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronDown, faTrash, faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons'
 import { v4 as uuidv4 } from 'uuid';
@@ -21,12 +22,13 @@ export const getServerSideProps = async () => {
 const IndexPage = ({ initialTodos }) => {
   const [inputValue, setInputValue] = useState('')
   const [todos, setTodos] = useState(initialTodos)
-  const read = useSelector(reading)
-  const numberTasks = useSelector(tasksLengthAll)
-  const numberIncompleteTasks = useSelector(tasksLengthIncomplete)
-  const numberCompleteTasks = useSelector(tasksLengthComplete)
-  const dispatch = useDispatch()
-  let j;
+  const [read, setRead] = useState("all")
+  const [numberTodos, setNumberTodos] = useState(initialTodos.length)
+  // const numberTasks = useSelector(tasksLengthAll)
+  // const numberIncompleteTasks = useSelector(tasksLengthIncomplete)
+  // const numberCompleteTasks = useSelector(tasksLengthComplete)
+  // const dispatch = useDispatch()
+  // let j;
   let id;
 
   const handleSubmit = async e => {
@@ -37,7 +39,7 @@ const IndexPage = ({ initialTodos }) => {
     })
     let newId = parseInt((id.join("")), 10)
     newId = Math.floor(newId / 1000000000000000000000000000000)
-    setTodos([{ id: newId, content: inputValue, done: false }, ...todos])
+    setTodos([...todos, { id: newId, content: inputValue, done: false }])
     await saveTodo({ id: newId, content: inputValue, done: false })
     setInputValue('')
   }
@@ -55,9 +57,15 @@ const IndexPage = ({ initialTodos }) => {
   }
 
   const toggleTodo = async todoFiltered => {
-    const todosFiltered = initialTodos.filter(todo => todo.id !== todoFiltered.id)
-    todoFiltered.done = true
-    setTodos([...todosFiltered, todoFiltered])
+    const todosFiltered = initialTodos.filter(todo => {
+      if (todo.id !== todoFiltered.id) {
+        return todo
+      } else {
+        todo.done = !(todo.done)
+        return todo
+      }
+    })
+    setTodos([...todosFiltered])
     await updateTodo(todoFiltered)
   }
 
@@ -67,15 +75,21 @@ const IndexPage = ({ initialTodos }) => {
       body: todo.id
     })
     const data = await response.json()
-    console.log(data)
   }
 
   const toggleReading = async r => {
-    // const todosFiltered = initialTodos.filter(todo => todo.id !== r)
-    // const todoFiltered = initialTodos.filter(todo => todo.id === r)
-    // todoFiltered.done = !todoFiltered.done
-    // setTodos([...todosFiltered, todoFiltered])
-    // await updateTodo(r)
+    setRead(r)
+    let numberTodos = 0;
+    initialTodos.map((todo) => {
+      if (r === "all") {
+        return numberTodos++
+      } else if (r === "done" && todo.done === true) {
+        return numberTodos++
+      } else if (r === "undone" && todo.done === false) {
+        return numberTodos++
+      }
+    })
+    setNumberTodos(numberTodos)
   }
 
   const deleteTodo = async (todoId) => {
@@ -84,91 +98,92 @@ const IndexPage = ({ initialTodos }) => {
       body: todoId
     })
     const data = await response.json()
-    console.log(data)
   }
 
-
-
   const removeTodo = async t => {
-    const todosFiltered = initialTodos.filter(todo => todo.id !== t)
+    const todosFiltered = initialTodos.filter(todo => {
+      if (todo.id !== t) {
+        return todo
+      }
+    })
     setTodos(todosFiltered)
     await deleteTodo(t)
   }
 
   const task = t => {
     return (
-      <div className={`${style.horizontalOrFlexOnly} ${style.paddingEdge} ${style.alignItemsCenter} ${style.greySolidTopBottom} ${style.justifyContentFlexStart} ${style.backgroundTodosHalf}`} key={t.id}>
+      <div className={style.task} key={t.id}>
         <input type="radio" onChange={() => toggleTodo(t)} checked={t.done ? true : false} />
         <p>{t.content}</p>
-        <button className={`${style.deleteButton}`} onClick={() => removeTodo(t.id)}><FontAwesomeIcon icon={faTrash} /></button>
-      </div >
+        <button className={style.deleteButton} onClick={() => removeTodo(t.id)}><FontAwesomeIcon icon={faTrash} /></button>
+      </div>
     )
   }
 
   return (
-    <div className={`${style.horizontalOrFlexOnly} ${style.all}`}>
-      <div className={`${style.menu}`}>
-        <div className={`${style.menuFortyHigh}`}>
-          <div className={`${style.horizontalOrFlexOnly} ${style.flexDirectionColumn} ${style.paddingEdge} ${style.titles}`}>
-            <h2 className={`${style.titlesBiggest}`}>React</h2>
-            <h3 className={`${style.smallTitles} ${style.titlesMedium}`}>Example</h3>
-            <p className={`${style.titlesSmallest}`}>Source</p>
-            <h3 className={`${style.smallTitles} ${style.titlesMedium}`}>React + Backbone.js</h3>
-            <p className={`${style.titlesSmallest}`}>Demo, Source</p>
-            <h3 className={`${style.smallTitles} ${style.titlesMedium}`}>Scala.js + React</h3>
-            <p className={`${style.titlesSmallest}`}>Demo, Source</p>
-            <h3 className={`${style.smallTitles} ${style.titlesMedium}`}>TypeScript + React</h3>
-            <p className={`${style.titlesSmallest}`}>Demo, Source</p>
-            <h3 className={`${style.smallTitles} ${style.titlesMedium}`}>React + Alt</h3>
-            <p className={`${style.titlesSmallest}`}>Demo, Source</p>
+    <div className={style.entirePage}>
+      <div className={style.menu}>
+        <div className={style.menuFortyHigh}>
+          <div className={style.firstRowMenu}>
+            <h2 className={style.titlesBiggest}>React</h2>
+            <h3 className={style.titlesMedium}>Example</h3>
+            <p className={style.titlesSmallest}>Source</p>
+            <h3 className={style.titlesMedium}>React + Backbone.js</h3>
+            <p className={style.titlesSmallest}>Demo, Source</p>
+            <h3 className={style.titlesMedium}>Scala.js + React</h3>
+            <p className={style.titlesSmallest}>Demo, Source</p>
+            <h3 className={style.titlesMedium}>TypeScript + React</h3>
+            <p className={style.titlesSmallest}>Demo, Source</p>
+            <h3 className={style.titlesMedium}>React + Alt</h3>
+            <p className={style.titlesSmallest}>Demo, Source</p>
           </div>
         </div>
         <div
-          className={`${style.menuTwentyHigh} ${style.horizontalOrFlexOnly} ${style.dottedTopBottom} ${style.alignItemsCenter} ${style.justifyContentSpaceAround} ${style.flexDirectionColumn} ${style.paddingTopAndBottom}`}>
+          className={style.bubbleRowMenu}>
           <FontAwesomeIcon icon={faQuoteLeft} className={style.faQuoteLeft} />
-          <div className={`${style.speechBubbleClippy} ${style.horizontalOrFlexOnly} ${style.justifyContentSpaceAround}`}>
-            <p className={`${style.speechBubbleClippyText}`}>React is a JavaScript library for creating user interfaces.Its
+          <div className={style.speechBubbleClippy}>
+            <p className={style.speechBubbleClippyText}>React is a JavaScript library for creating user interfaces.Its
               core principles are declarative code,
               efficiency, and flexibility.Simply specify what your component looks like and React will keep it
               up-to-date when the underlying data changes.</p>
           </div>
           <FontAwesomeIcon icon={faQuoteRight} className={style.faQuoteRight} />
-          <div className={`${style.horizontalOrFlexOnly} ${style.reactLabelSpace} ${style.justifyContentFlexEnd} ${style.alignItemsCenter} ${style.wider}`}>
-            <p className={`${style.reactLabel}`}>React</p>
+          <div className={style.reactLabelSpace}>
+            <p className={style.reactLabel}>React</p>
           </div>
         </div>
-        <div className={`${style.menuThirtyFiveHigh}`}>
+        <div className={style.menuThirtyFiveHigh}>
           <div
-            className={`${style.horizontalOrFlexOnly} ${style.menuHalfHigh} ${style.justifyContentSpaceAround} ${style.flexDirectionColumn} ${style.paddingEdge} ${style.paddingTopAndBottom}`}>
-            <h2 className={`${style.titlesBiggest}`}>Official Resources</h2>
-            <p className={`${style.titlesSmallest}`}>Tutorial</p>
-            <p className={`${style.titlesSmallest}`}>Philosophy</p>
-            <p className={`${style.titlesSmallest}`}>Support</p>
-            <p className={`${style.titlesSmallest}`}>Flux architecture example</p>
+            className={style.listBottomThirdMenu}>
+            <h2 className={style.titlesBiggest}>Official Resources</h2>
+            <p className={style.titlesSmallest}>Tutorial</p>
+            <p className={style.titlesSmallest}>Philosophy</p>
+            <p className={style.titlesSmallest}>Support</p>
+            <p className={style.titlesSmallest}>Flux architecture example</p>
           </div>
           <div
-            className={`${style.horizontalOrFlexOnly} ${style.menuHalfHigh} ${style.justifyContentSpaceAround} ${style.flexDirectionColumn} ${style.paddingEdge}`}>
-            <h2 className={`${style.titlesBiggest}`}>Community</h2>
-            <p className={`${style.titlesSmallest}`}>ReactJS on Stack Overflow</p>
-            <p className={`${style.titlesSmallest}`}>Google Groups Mailing List</p>
-            <p className={`${style.titlesSmallest}`}>IRC</p>
+            className={style.listBottomThirdMenu}>
+            <h2 className={style.titlesBiggest}>Community</h2>
+            <p className={style.titlesSmallest}>ReactJS on Stack Overflow</p>
+            <p className={style.titlesSmallest}>Google Groups Mailing List</p>
+            <p className={style.titlesSmallest}>IRC</p>
           </div>
         </div>
       </div>
-      <div className={`${style.horizontalOrFlexOnly} ${style.background} ${style.alignItemsCenter} ${style.flexDirectionColumn}`}>
+      <div className={style.mainBackground}>
         <div
-          className={`${style.horizontalOrFlexOnly} ${style.backgroundItems} ${style.justifyContentSpaceBetween} ${style.flexDirectionColumn}`}>
-          <div className={`${style.backgroundTitle} ${style.horizontalOrFlexOnly} ${style.justifyContentSpaceAround}`}>
-            <h1 className={`${style.bigTitles}`}>todos</h1>
+          className={style.grid}>
+          <div className={style.firstGrid}>
+            <h1 className={style.bigTitles}>todos</h1>
           </div>
-          <div className={`${style.backgroundTodos} ${style.horizontalOrFlexOnly} ${style.flexDirectionColumn} ${style.flexGrow10} ${style.dropShadowEdge}`}>
-            <form onSubmit={handleSubmit} className={`${style.horizontalOrFlexOnly} ${style.backgroundTodosForm} ${style.dropShadowEdge}`}>
-              <div className={`${style.horizontalOrFlexOnly} ${style.paddingEdge} ${style.alignItemsCenter}`}>
+          <div className={style.secondGrid}>
+            <form onSubmit={handleSubmit} className={style.form}>
+              <div className={style.formField}>
                 <FontAwesomeIcon icon={faChevronDown} />
-                <input value={inputValue} name="todo" className={`${style.noOutline}`} placeholder="What needs to be done?" onChange={e => setInputValue(e.target.value)} />
+                <input value={inputValue} name="todo" className={style.noOutline} placeholder="What needs to be done?" onChange={e => setInputValue(e.target.value)} />
               </div>
             </form>
-            <div className={`${style.horizontalOrFlexOnly} ${style.backgroundTodosList} ${style.flexDirectionColumn} ${style.flexGrow10}`}>
+            <div className={style.todoArea}>
               {
                 todos ?
                   todos.map(t => {
@@ -181,37 +196,37 @@ const IndexPage = ({ initialTodos }) => {
                     }
                   }
                   ) :
-                  <div className={`${style.horizontalOrFlexOnly} ${style.paddingEdge} ${style.alignItemsCenter} ${style.greySolidTopBottom} ${style.justifyContentFlexStart} ${style.backgroundTodosHalf}`}>
+                  <div className={style.listTasks}>
                     <p>No tasks</p>
                   </div>
               }
               <div
-                className={`${style.horizontalOrFlexOnly} ${style.paddingEdge} ${style.alignItemsCenter} ${style.greySolidTopBottom} ${style.justifyContentFlexStart} ${style.backgroundTodosHalf}`}>
+                className={style.lastBoxTodos}>
               </div>
             </div>
             <div
-              className={`${style.horizontalOrFlexOnly} ${style.backgroundTodosFooter} ${style.justifyContentFlexStart} ${style.alignItemsCenter}`}>
-              <div className={`${style.horizontalOrFlexOnly} ${style.justifyContentFlexStart} ${style.halfWidth} ${style.paddingEdge}`}>
-                <p>{initialTodos.length} Items</p>
+              className={style.innerLastBoxTodos}>
+              <div className={style.itemLengthBox}>
+                <p>{numberTodos} Items</p>
               </div>
-              <div className={`${style.horizontalOrFlexOnly} ${style.justifyContentFlexStart} ${style.halfWidth}`}>
-                <div className={`${style.horizontalOrFlexOnly} ${style.justifyContentSpaceAround} ${style.stateTasks}`}>
-                  <button onClick={() => toggleReading("all")} className={read === "all" ? `${style.buttonModeActive} ${style.stateTasksButtons}` : `${style.buttonMode} ${style.stateTasksButtons}`}>All</button>
-                  <button onClick={() => toggleReading("undone")} className={read === "undone" ? `${style.buttonModeActive} ${style.stateTasksButtons}` : `${style.buttonMode} ${style.stateTasksButtons}`}>Active</button>
-                  <button onClick={() => toggleReading("done")} className={read === "done" ? `${style.buttonMode} ${style.stateTasksButtons}` : `${style.buttonModeActive} ${style.stateTasksButtons}`}> Completed</button >
+              <div className={style.boxButtonsState}>
+                <div className={style.buttonsState}>
+                  <button onClick={() => toggleReading("all")} className={read === "all" ? style.buttonActive : style.buttonInactive}>All</button>
+                  <button onClick={() => toggleReading("undone")} className={read === "undone" ? style.buttonActive : style.buttonInactive}>Active</button>
+                  <button onClick={() => toggleReading("done")} className={read === "done" ? style.buttonActive : style.buttonInactive}> Completed</button>
                 </div>
               </div>
             </div>
           </div>
           <div
-            className={`${style.horizontalOrFlexOnly} ${style.flexDirectionColumn} ${style.justifyContentSpaceAround} ${style.alignItemsCenter} ${style.backgroundCredits}`}>
-            <p className={`${style.credits}`}>Double-click to edit a todo</p>
-            <p className={`${style.credits}`}>Created by Jo</p>
-            <p className={`${style.credits}`}>Part of TodoMVC</p>
-          </div >
-        </div >
-      </div >
-    </div >
+            className={style.creditRoll}>
+            <p className={style.credits}>Double-click to edit a todo</p>
+            <p className={style.credits}>Created by Jo</p>
+            <p className={style.credits}>Part of TodoMVC</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
